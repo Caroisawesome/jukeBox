771483587ea5ef8713825be46c1e4d3eeefcb969
Winston Riley <winston@mindsharestudios.com>
2016-08-08 17:19:29 -0600

    add less and basic styles

--- app/client/imports/App.js
+++ app/client/imports/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import TopNavBar from './containers/TopNavBar.js';
 import Room from './containers/RoomView';
 
 
 class App extends Component {
     render() {
         return (
-            <div className="ui container">
-              <Room />
+            <div className="wrapper">
+              <TopNavBar />
+
+
+              <div className="ui container">
+                <Room />
+              </div>
+
             </div>
         );
     }
