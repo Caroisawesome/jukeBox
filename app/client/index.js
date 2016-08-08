2f5f2b27f94d74f1744fc1f2854ebff803698a0a
Winston Riley <winston@mindsharestudios.com>
2016-08-08 15:29:28 -0600

    redux architecture is up and running

--- app/client/index.js
+++ app/client/index.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
 import App from './imports/App.js';
 
+
+import store from './imports/store.js';
+
+
+const Main = () => {
+    return (
+        <Provider store={store}>
+          <App />
+        </Provider>
+    );
+};
+
 Meteor.startup(() => {
-    render(<App />, document.getElementById('app'));
+    render(<Main />, document.getElementById('app'));
 });
