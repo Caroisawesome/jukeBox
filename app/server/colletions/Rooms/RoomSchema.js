de1d671c73720dc22ac7d088f73d1d60955c6893
Carolyn Atterbury <carolynatterbury@gmail.com>
2016-08-15 15:38:16 -0600

    added collections and schemas for UserQueues and Rooms

---
+++ app/server/colletions/Rooms/RoomSchema.js
@@ -0,0 +1,22 @@
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { attachSchema } from 'meteor/aldeed:collection2';
+import { Mongo } from 'meteor/mongo';
+
+const RoomSchema = new SimpleSchema({
+    adminId: {
+        type: String,
+    },
+
+    /**
+     * id of spotify playlist?  Songs will be added to here
+     */
+    playlistId: {
+        type: String,
+    }
+});
+
+const Rooms = new Mongo.Collection('rooms');
+
+Rooms.attachSchema(RoomSchema);
+
+export { Rooms, RoomSchema };
